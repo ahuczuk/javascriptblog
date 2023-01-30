6e33596f604a71f423985ade6da0dc360ce8e84a
Agata Huczuk <agatahuczuk@cosibella.pl>
2023-01-30 17:42:41 +0100

    modified html js

--- js/script.js
+++ js/script.js
@@ -35,7 +35,8 @@ function titleClickHandler(event) {
 const
   optArticleSelector = '.post',
   optTitleSelector = '.post-title',
-  optTitleListSelector = '.titles';
+  optTitleListSelector = '.titles',
+  optArticleTagsSelector = '.post-tags .list';
 
 function generateTitleLinks(customSelector = '') {
 
@@ -79,3 +80,45 @@ function generateTitleLinks(customSelector = '') {
 }
 
 generateTitleLinks();
+
+
+function generateTags() {
+  /* find all articles */
+
+  const articles = document.querySelectorAll(optArticleTagsSelector);
+
+  /* START LOOP: for every article: */
+
+  for (let article of articles) {
+
+    /* find tags wrapper */
+
+    const tagsWrapper = article.querySelector(opts.articleTagsSelector);
+    tagsWrapper.innerHTML = '';
+
+
+    /* make html variable with empty string */
+    let html = '';
+
+    /* get tags from data-tags attribute */
+
+    const articleTags = article.getAttribute('data-tags');
+    console.log(articleTags);
+
+    /* split tags into array */
+
+    /* START LOOP: for each tag */
+
+    /* generate HTML of the link */
+
+    /* add generated code to html variable */
+
+    /* END LOOP: for each tag */
+
+    /* insert HTML of all the links into the tags wrapper */
+
+    /* END LOOP: for every article: */
+  }
+
+  generateTags();
+}
